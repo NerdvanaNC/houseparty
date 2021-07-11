184367a829e0aeed62c67b84e790a2f476642666
NerdvanaNC <nickunjchopra@gmail.com>
2021-07-11 18:34:39 +0530

    Setup the Create Room page  with material-ui components and learnt about state in React. Create Room functionality now functional with it sending a request to the 'create-room' endpoint we made.

--- api/models.py
+++ api/models.py
@@ -1,9 +1,12 @@
 from django.db import models
 from django.utils.crypto import get_random_string
 
+def room_code_generator():
+  return get_random_string(length=6).upper()
+
 # Create your models here.
 class Room(models.Model):
-  code = models.CharField(max_length=8, default=get_random_string(length=6), unique=True)
+  code = models.CharField(max_length=8, default=room_code_generator, unique=True)
   host = models.CharField(max_length=50, unique=True)
   guest_can_pause = models.BooleanField(null=False, default=False)
   votes_to_skip = models.IntegerField(null=False, default=1)
