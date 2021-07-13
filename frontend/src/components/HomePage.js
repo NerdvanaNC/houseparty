0efc57e74b886e1921f1b249d69610d43e6120bc
NerdvanaNC <nickunjchopra@gmail.com>
2021-07-13 19:58:43 +0530

    Created a GetRoom endpoint,  made a Component for the Room itself. Hits up the endpoint and gets the details for the room. CreateRoom component now redirects the user to the newly-created room upon submit.

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Switch, Route, Link, Redirect } from 'react-router-dom'
 import RoomJoinPage from './RoomJoinPage';
 import CreateRoomPage from './CreateRoomPage';
+import Room from './Room';
 
 export default function HomePage(props) {
   return (
@@ -12,6 +13,7 @@ export default function HomePage(props) {
         </Route>
         <Route path='/join' component={RoomJoinPage} />
         <Route path='/create' component={CreateRoomPage} />
+        <Route path='/room/:roomCode' component={Room} />
       </Switch>
     </Router>
   )
